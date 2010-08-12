9c8ab811b19be859facd34b3b2e473b4c5acd3c7
serch <serch@localhost>
2010-08-12 00:30:59 -0300

    initial drafts and skeleton

---
+++ rl.bomber/Agent.py
@@ -0,0 +1,17 @@
+class Agent:
+	
+	def __init__(self, stateCount, actionCount):
+
+		self.stateCount = stateCount
+		self.actionCount = actionCount
+
+	
+	def learn(self, stateCode, reward):
+		
+		# process state and reward
+		print "learning..."
+		
+	def nextAction(self):
+		
+		# return next action
+		return 0
