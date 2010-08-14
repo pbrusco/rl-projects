5db88f31e07dae13901bc7c210a03647164e7ccd
ataraciuk <ataraciuk@gmail.com>
2010-08-14 17:50:46 -0300

    Tests

--- rl.tests/EnvironmentFixture.py
+++ rl.tests/EnvironmentFixture.py
@@ -12,18 +12,50 @@ class TestEnvironment(unittest.TestCase):
 	
 	def test_simple_nav(self):
 		self.move(Action.RIGHT)
-		self.assertPos(1,0)
+		self.assertPos(0,1)
 		self.move(Action.RIGHT)
-		self.assertPos(2,0)
+		self.assertPos(0,2)
 		self.move(Action.DOWN)
-		self.assertPos(2,1)
+		self.assertPos(1,2)
 		self.move(Action.UP)
-		self.assertPos(2,0)
+		self.assertPos(0,2)
 		self.move(Action.LEFT)
-		self.assertPos(1,0)
+		self.assertPos(0,1)
+	
+	def test_nav_top_edge(self):
+		self.move(Action.LEFT)
+		self.assertPos(0,0)
+		self.move(Action.UP)
+		self.assertPos(0,0)
+		self.move(Action.RIGHT)
+		self.assertPos(0,1)
+		self.move(Action.UP)
+		self.assertPos(0,1)
+		for i in range(10): self.move(Action.RIGHT)
+		self.assertPos(0,7)
+		self.move(Action.UP)
+		self.assertPos(0,7)
 		
+	def test_nav_bottom_edge(self):
+		self.setPos(7,0)
+		self.move(Action.LEFT)
+		self.assertPos(7,0)
+		self.move(Action.DOWN)
+		self.assertPos(7,0)
+		self.move(Action.RIGHT)
+		self.assertPos(7,1)
+		self.move(Action.DOWN)
+		self.assertPos(7,1)
+		for i in range(10): self.move(Action.RIGHT)
+		self.assertPos(7,7)
+		self.move(Action.DOWN)
+		self.assertPos(7,7)
+	
 	def move(self,dir):
 		return self.env.performAction(dir)
+		
+	def setPos(self,i,j):
+		return self.env.state.bombermanPos = (i,j)
 
-	def assertPos(self,x,y):
-		self.assertEqual(self.env.state.bombermanPos,(x,y))
+	def assertPos(self,i,j):
+		self.assertEqual(self.env.state.bombermanPos,(i,j))
