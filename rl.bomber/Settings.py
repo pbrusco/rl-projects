a05b3274de505615e91d6fcee383054970d5dad1
carolinahadad <carolinahadad@carito.local>
2010-08-14 17:56:12 -0300

    changed x,y to row,col

---
+++ rl.bomber/Settings.py
@@ -0,0 +1,17 @@
+# Bomberman Settings
+
+BOARD_HEIGHT = 8
+BOARD_WIDTH = 8
+EXIT = (7,7)
+STONES = [(2,0),(2,2),(2,4),(2,6),(4,0),(4,2),(4,4),(4,6),(6,0),(6,2),(6,4),(6,6)]
+WALLS = [(1,2),(1,3),(1,5),(1,7),(3,2),(3,3),(3,5),(3,7),(5,2),(5,3),(5,5),(5,7)]
+
+# RL Settings
+
+WIN_REWARD = 10
+LOSE_REWARD = -100
+
+# Manager Settings
+
+ITERATIONS = 100
+MAX_TURNS = 1000
