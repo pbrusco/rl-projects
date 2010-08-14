7bc5c4ddf95eda65c818465bebd94f5cd18797c3
pb <pb@localhost>
2010-08-14 17:37:11 -0500

    visual staff

---
+++ rl.bomber/Settings.py
@@ -0,0 +1,34 @@
+# Bomberman Settings
+
+MAP_SIZE = 3
+
+if MAP_SIZE == 3:
+	BOARD_HEIGHT = 3
+	BOARD_WIDTH = 3
+	EXIT = (2,2)
+	STONES = [(1,2),(2,1)]
+	WALLS = [(1,1)]
+
+elif MAP_SIZE == 5:
+	BOARD_HEIGHT = 5
+	BOARD_WIDTH = 5
+	EXIT = (4,4)
+	STONES = [(2,0),(2,2),(2,4),(4,0),(4,2)]
+	WALLS = [(1,1),(1,3),(3,1),(3,3)]
+	
+elif MAP_SIZE == 8:
+	BOARD_HEIGHT = 8
+	BOARD_WIDTH = 8
+	EXIT = (7,7)
+	STONES = [(2,0),(2,2),(2,4),(2,6),(4,0),(4,2),(4,4),(4,6),(6,0),(6,2),(6,4),(6,6)]
+	WALLS = [(1,1),(1,3),(1,5),(1,7),(3,1),(3,3),(3,5),(3,7),(5,1),(5,3),(5,5),(5,7)]
+
+# RL Settings
+
+WIN_REWARD = 10
+LOSE_REWARD = -100
+
+# Manager Settings
+
+ITERATIONS = 100
+MAX_TURNS = 1000
