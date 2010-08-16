61a299712ce8725ba7b10924e50234108f5bae42
Santiago Palladino <spalladino@gmail.com>
2010-08-16 20:44:46 -0300

    Hash tests

---
+++ rl.bomber/Agent.py
@@ -0,0 +1,39 @@
+import random
+import Print
+
+from Action import *
+
+ALPHA = 0.8
+GAMMA = 0.95
+EPSILON = 0.1
+ACTIONS = Action.ACTIONS
+
+class Agent:
+
+	def __init__(self):
+		self.qTable = {}
+	
+	def learn(self, previous, state, action, reward):
+		previousValue = self.getQValue(action,previous)
+		value = previousValue + ALPHA*(reward + GAMMA*(max([self.getQValue(a,state) for a in ACTIONS])) - previousValue)
+		self.setQValue(action,previous,value)
+		#print self.qTable
+		
+	def nextAction(self,state):
+		if self.goRandom(): 
+			return random.choice(ACTIONS)
+		else: 
+			qMax = max([self.getQValue(a,state) for a in Action.ACTIONS])	
+			return random.choice([a for a in Action.ACTIONS if self.getQValue(a,state)==qMax])
+
+	def goRandom(self):
+		return random.random() < EPSILON
+
+	def getQValue(self,action,state):
+		return self.qTable.get((action,int(state))) or 0.0 #float!
+		
+	def setQValue(self,action,state,value):
+ 		self.qTable[(action,int(state))] = value
+
+	def inspect(self):
+		return Print.prnDict(self.qTable)
---
+++ rl.tests/TaskRewardsFixture.py
@@ -0,0 +1,104 @@
+import unittest
+import sys
+
+sys.path.append('../rl.bomber')
+
+from Environment import Environment
+from Task import Task
+
+from Action import *
+from Settings import *
+from Maps import *
+
+class TestRewards(unittest.TestCase):
+	
+	def setUp(self):
+		self.env = Environment()
+		self.task = Task(self.env)
+	
+	def test_settings(self):
+		self.assertEquals(MAP_SIZE, 8)
+		self.assertEquals(IS_IMMORTAL, False)
+	
+	def test_simple_nav(self):
+		if (NAVIGATION_REWARD == NAVIGATION_REWARD_PROPORTIONAL_TO_EXIT and BOMB_REWARD_POLICY == BOMB_NO_REWARD):
+			self.perform(Action.RIGHT, 1)
+			self.perform(Action.RIGHT, 2)
+			self.perform(Action.LEFT, 1)
+			self.perform(Action.LEFT, 0)
+		
+			self.perform(Action.RIGHT, 1)
+			self.perform(Action.DOWN, 0) #wall position didn't change
+		
+			self.perform(Action.RIGHT, 2)
+			self.perform(Action.DOWN, 3)		
+			self.perform(Action.DOWN, 0) #wall position didn't change
+			self.perform(Action.RIGHT, 0) #wall position didn't change
+			
+		
+	def test_bombs_reward(self):
+		if (NAVIGATION_REWARD == NAVIGATION_NO_REWARD and BOMB_REWARD_POLICY == BOMB_REWARD_PER_STONE_DESTROYED):
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.DOWN, 0)
+			
+			self.perform(Action.DROP_BOMB, 0)
+			self.perform(Action.UP, 0)
+			self.perform(Action.LEFT, 0)
+			self.perform(Action.EXPLODE, 10) 
+			
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DROP_BOMB, 0)
+			self.perform(Action.EXPLODE, LOSE_REWARD) #agent die whitout reward for exploding bomb
+			
+	def test_bombs_reward_2(self):
+		if (NAVIGATION_REWARD == NAVIGATION_NO_REWARD and BOMB_REWARD_POLICY == BOMB_REWARD_PER_STONE_DESTROYED_PROPORTIONAL_TO_EXIT):	
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.DOWN, 0)
+			
+			self.perform(Action.DROP_BOMB, 0)
+			self.perform(Action.UP, 0)
+			self.perform(Action.LEFT, 0)
+			self.perform(Action.EXPLODE, 10 * 4) 
+			
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DROP_BOMB, 0)
+			self.perform(Action.UP, 0)
+			self.perform(Action.UP, 0)
+			self.perform(Action.EXPLODE, 10 * 6) 
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DROP_BOMB, 0)
+			self.perform(Action.UP, 0)
+			self.perform(Action.UP, 0)
+			self.perform(Action.EXPLODE, 10 * 8)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.DOWN, 0)
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.RIGHT, 0)
+			self.perform(Action.RIGHT, WIN_REWARD)
+		
+		if (NAVIGATION_REWARD == NAVIGATION_NO_REWARD and BOMB_REWARD_POLICY == BOMB_NO_REWARD):
+			statePrev, rewardPrev, statusPrev = self.task.perform(Action.RIGHT)
+			state, reward, status = self.task.perform(Action.EXPLODE)
+			self.assertEquals(state, statePrev)
+			
+		
+	def perform(self, action, expectedReward):
+		state, reward, status = self.task.perform(action)
+		self.assertEquals(reward, expectedReward)
+		
+	
