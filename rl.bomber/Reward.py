3f57627871ac0c864688659ea17bc86b81d1ee0f
Santiago Palladino <spalladino@gmail.com>
2010-08-16 17:10:32 -0300

    Max rewards

--- rl.bomber/Reward.py
+++ rl.bomber/Reward.py
@@ -1,3 +1,7 @@
+from Constants import *
+from Settings import *
+from Task import *
+
 class Reward:
 
 	DEAD = 0
@@ -6,8 +10,8 @@ class Reward:
 	
 	REWARDS = [DEAD, POSITION, STONE]
 	
-	def __init__(self, kinds=None):
-		self.kinds = kinds or {}
+	def __init__(self, kind=None,value=0.0):
+		self.kinds = {kind: value} if not kind is None else {}
 		
 	def __float__(self):
 		return float(sum(self.kinds.values()))
@@ -17,10 +21,34 @@ class Reward:
 	
 	def getRewardForFactor(self, factor):
 		return self.kinds.get(factor) or 0.0
-		
-	def addRewardForFactor(self, factor, value):
-		self.kinds[factor] = self.getRewardForFactor(factor) + value
-		
-	def maxRewardForFactor(self, factor):
-		# TODO: Which is max reward for each factor?
-		return 0.0
+	
+	@classmethod
+	def getRewardForBombPosition(cls,bombPosition):
+		if (BOMB_REWARD_POLICY == BOMB_REWARD_PER_STONE_DESTROYED_PROPORTIONAL_TO_EXIT):
+			closenessX = abs(MAP_SIZE-1 -(EXIT[0] - bombPosition[0])) 
+			closenessY = abs(MAP_SIZE-1 -(EXIT[1] - bombPosition[1]))
+			closeness = closenessX + closenessY
+			return closeness * BOMB_REWARD
+		if (BOMB_REWARD_POLICY == BOMB_REWARD_PER_STONE_DESTROYED):
+			return BOMB_REWARD
+		return 0
+	
+	@classmethod
+	def getRewardForAgentPosition(cls,agentPosition):
+		closenessX = abs(MAP_SIZE-1 -(EXIT[0] - agentPosition[0])) 
+		closenessY = abs(MAP_SIZE-1 -(EXIT[1] - agentPosition[1]))
+		closeness = closenessX + closenessY
+		return closeness
+	
+	
+class MaxReward:
+	
+	MAX_REWARDS = {
+				Reward.DEAD: 0, 
+				Reward.POSITION: WIN_REWARD, 
+				Reward.STONE: max([Reward.getRewardForBombPosition(stone) for stone in STONES])
+				}
+
+	@classmethod
+	def forFactor(cls, factor):
+		return MaxReward.MAX_REWARDS[factor]
--- rl.bomber/Task.py
+++ rl.bomber/Task.py
@@ -16,43 +16,29 @@ class Task(object):
 	def perform(self,action):
 		bombsExploded = self.env.performAction(action)
 		
-		reward = Reward()
+		reward = None
 		status = Status.CONTINUE
 		state = self.env.state
 		
 		# Intermediate
 		if (BOMB_REWARD_POLICY != BOMB_NO_REWARD):
-			for i in range(len(bombsExploded)):
-				reward.addRewardForFactor(Reward.STONE, self.getRewardForBombPosition(bombsExploded[i]))
+			value = sum([Reward.getRewardForBombPosition(b) for b in bombsExploded])
+			reward = Reward(Reward.STONE, value)
 				
 		if (NAVIGATION_REWARD != NAVIGATION_NO_REWARD):
 			if (self.env.positionChangedInLastAction == True):
-				reward.addRewardForFactor(Reward.POSITION, self.getRewardForAgentPosition(state.bombermanPos))
+				reward = Reward(Reward.POSITION, Reward.getRewardForAgentPosition(state.bombermanPos))
 			
 		if (state.bombermanPos == EXIT):
-			reward.addRewardForFactor(Reward.POSITION, WIN_REWARD)
+			reward = Reward(Reward.POSITION, WIN_REWARD)
 			status = Status.WIN
 		if (state.die):
-			reward.addRewardForFactor(Reward.DEAD, LOSE_REWARD)
+			reward = Reward(Reward.DEAD, LOSE_REWARD)
 			status = Status.DIE
 
-		return (state, reward, status)
+		return (state, reward or Reward(), status)
 		
 	def getState(self):
 		return self.env.state
 		
-	def getRewardForBombPosition(self, bombPosition):
-		if (BOMB_REWARD_POLICY == BOMB_REWARD_PER_STONE_DESTROYED_PROPORTIONAL_TO_EXIT):
-			closenessX = abs(MAP_SIZE-1 -(EXIT[0] - bombPosition[0])) 
-			closenessY = abs(MAP_SIZE-1 -(EXIT[1] - bombPosition[1]))
-			closeness = closenessX + closenessY
-			return closeness * BOMB_REWARD
-		if (BOMB_REWARD_POLICY == BOMB_REWARD_PER_STONE_DESTROYED):
-			return BOMB_REWARD
-		return 0
-		
-	def getRewardForAgentPosition(self, agentPosition):
-		closenessX = abs(MAP_SIZE-1 -(EXIT[0] - agentPosition[0])) 
-		closenessY = abs(MAP_SIZE-1 -(EXIT[1] - agentPosition[1]))
-		closeness = closenessX + closenessY
-		return closeness
+	
