3e7b4146e7a4a699e1924a04248e9e28fe032fa3
spalladino <spalladino@localhost>
2010-08-18 22:59:25 -0300

    Fixed dyna

--- rl.bomber/DynaAgent.py
+++ rl.bomber/DynaAgent.py
@@ -2,8 +2,6 @@ import random
 from Action import *
 import Print
 
-
-
 LEARNING_RATE = 0.8
 DISCOUNT_FACTOR = 0.95
 EPSILON = 0.1
@@ -11,11 +9,11 @@ N = 1000
 
 class DynaAgent:
 
-    def __init__(self):
-        self.qTable = {}
-        self.model = {} 
-    
-    def learn(self, state, nextState, action, reward, nextChosenAction):
+	def __init__(self):
+		self.qTable = {}
+		self.model = {} 
+	
+	def learn(self, state, nextState, action, reward, nextChosenAction):
 
 		previousQValue = self.getQValue(action,state) #valor previo de Q(s,a)
 		maxFutureValue = max([self.getQValue(a,nextState) for a in Action.ACTIONS]) #valor futuro maximo desde el nuevo estado
@@ -23,12 +21,12 @@ class DynaAgent:
 		
 		self.setQValue(action,state, previousQValue + LEARNING_RATE*(expectedDiscountedReward - previousQValue))
 		
-        self.setModelValue(action,state,(reward,nextState)) 
-        for i in range(N) #Expande lo aprendido en estados visitados para reforzar el aprendizaje sin tener que actuar en el mundo.
+		self.setModelValue(action,state,(reward,nextState)) 
+		for i in range(N): #Expande lo aprendido en estados visitados para reforzar el aprendizaje sin tener que actuar en el mundo.
 			randomVisited = random.choice(self.qTable.keys())
 			randomVisitedState = randomVisited[1]
 			randomVisitedAction = randomVisited[0]
-			oldReward,oldNextState = self.getModelValue(randomVisitedAction,randomVisitedState):
+			oldReward,oldNextState = self.getModelValue(randomVisitedAction,randomVisitedState)
 			
 			previousQValue = self.getQValue(randomVisitedAction,randomVisitedState) 
 			maxFutureValue = max([self.getQValue(a,oldNextState) for a in Action.ACTIONS])
@@ -36,30 +34,27 @@ class DynaAgent:
 		
 			self.setQValue(randomVisitedAction,randomVisitedState, previousQValue + LEARNING_RATE*(expectedDiscountedReward - previousQValue))
 		
+	def nextAction(self,state):
+		if self.goRandom(): 
+			return random.choice(Action.ACTIONS)
+		else: 
+			qMax = max([self.getQValue(a,state) for a in Action.ACTIONS])	
+			return random.choice([a for a in Action.ACTIONS if self.getQValue(a,state)==qMax])
 
-        
-        
-    def nextAction(self,state):
-        if self.goRandom(): 
-            return random.choice(Action.ACTIONS)
-        else: 
-            qMax = max([self.getQValue(a,state) for a in Action.ACTIONS])    
-            return random.choice([a for a in Action.ACTIONS if self.getQValue(a,state)==qMax])
+	def goRandom(self):
+		return random.random() < EPSILON
 
-    def goRandom(self):
-        return random.random() < EPSILON
+	def getQValue(self,action,state):
+		return self.qTable.get((action,int(state))) or 0.0 #float!
+		
+	def setQValue(self,action,state,value):
+		self.qTable[(action,int(state))] = value
 
-    def getQValue(self,action,state):
-        return self.qTable.get((action,int(state))) or 0.0 #float!
-        
-    def setQValue(self,action,state,value):
-        self.qTable[(action,int(state))] = value
-
-    def setModelValue(self,action,state,value):
-		self.model[(action,int(state)) = (value[0],int(value[1]))
+	def setModelValue(self,action,state,value):
+		self.model[(action,int(state))] = (value[0],int(value[1]))
 		
 	def getModelValue(self,action,state):
-        return self.model.get((action,int(state))) or  (0.0,0.0)
+		return self.model.get((action,int(state))) or  (0.0,0.0)
 		
-    def inspect(self):
-        return Print.prnDict(self.qTable) + Print.prnDict(self.modelTable)
+	def inspect(self):
+		return Print.prnDict(self.qTable) + Print.prnDict(self.modelTable)
