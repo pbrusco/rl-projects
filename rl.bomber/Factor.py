a413f60a58a1f5aa6b60a13c691bc85685de17b1
Santiago Palladino <spalladino@gmail.com>
2010-08-21 18:01:51 -0300

    Max factor values

--- rl.bomber/Factor.py
+++ rl.bomber/Factor.py
@@ -1,4 +1,6 @@
-from Settings import USE_DELTABOMB_FACTOR
+from Settings import *
+
+import Maps
 
 class Factor:
 
@@ -9,3 +11,15 @@ class Factor:
 	DELTABOMB = 4
 
 	FACTORS = [POSITION,BOMB,STONES,DEAD,DELTABOMB] if USE_DELTABOMB_FACTOR else [POSITION,BOMB,STONES,DEAD] 
+	
+	MAX_FACTORS = {
+		POSITION: Maps.BOARD_HEIGHT * Maps.BOARD_WIDTH - len(Maps.WALLS),
+		BOMB: Maps.BOARD_HEIGHT * Maps.BOARD_WIDTH- len(Maps.WALLS),
+		STONES: len(Maps.STONES),
+		DEAD: 2,
+		DELTABOMB: Maps.BOARD_HEIGHT * Maps.BOARD_WIDTH
+	}
+	
+	@classmethod
+	def getMaxValues(cls, factor):
+		return Factor.MAX_FACTORS[factor]
