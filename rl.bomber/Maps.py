caf1c85195c9a0375b9cff3e99357a013149f6f2
Pablo Brusco <pablo.brusco@gmail.com>
2010-08-22 21:37:59 -0300

    pantallazos

---
+++ rl.bomber/Maps.py
@@ -0,0 +1,22 @@
+from Settings import *
+
+if MAP_SIZE == 3:
+	BOARD_HEIGHT = 3
+	BOARD_WIDTH = 3
+	EXIT = (2,2)
+	STONES = [(1,2),(2,1)]
+	WALLS = [(1,1)]
+
+elif MAP_SIZE == 5:
+	BOARD_HEIGHT = 5
+	BOARD_WIDTH = 5
+	EXIT = (4,4)
+	STONES = [(2,0),(2,2),(2,4),(4,0),(4,2)]
+	WALLS = [(1,1),(1,3),(3,1),(3,3)]
+	
+elif MAP_SIZE == 8:
+	BOARD_HEIGHT = 8
+	BOARD_WIDTH = 8
+	EXIT = (7,7)
+	STONES = [(2,0),(2,2),(2,4),(2,6),(4,0),(4,2),(4,4),(4,6),(6,0),(6,2),(6,4),(6,6)]
+	WALLS = [(1,1),(1,3),(1,5),(1,7),(3,1),(3,3),(3,5),(3,7),(5,1),(5,3),(5,5),(5,7)]
