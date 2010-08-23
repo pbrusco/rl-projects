3a82fc44c93279004c56f2f48e25a2bf2ff1afe6
pablo brusco <pablo.brusco@gmail.com>
2010-08-22 23:29:14 -0300

    future

---
+++ rl.stats/stats.py
@@ -0,0 +1,64 @@
+import os, fnmatch
+
+MAX_GAMES = 10000
+PERCENTAGE_OF_LOST_GAMES = 0.20
+WIN_STATUS = 3
+
+class GameData():
+	def __init__(self, splitteddata):
+		self.elapsed, self.turns, self.status, self.movementActionsCount, self.dropActionsCount, self.explodeActionsCount, self.noResultActionsCount = [float(data) for data in splitteddata]
+
+def calculateConvergence(games):
+	statuses = [g.status for g in games]
+	win = statuses.count(WIN_STATUS) 
+	lose = len(statuses) - win
+	haswon = False
+	
+	for i,status in enumerate(statuses):
+		if haswon and float(win) * PERCENTAGE_OF_LOST_GAMES > float(lose):
+			return None if MAX_GAMES - i < 50 else i
+		elif status == WIN_STATUS:
+			haswon = True
+			win -= 1
+		else:
+			lose -= 1
+		
+		
+	return None
+	
+		
+def printStats(filepath, games):
+	totalTime = float(sum([g.elapsed for g in games]))
+	totalMovements = float(sum([g.turns for g in games]))
+	timePerMovement = totalTime / totalMovements
+	convergence = calculateConvergence(games)
+	minmoves = min([g.turns for g in games if g.status == WIN_STATUS])
+	
+	print "{0} & {1:.2e} & {2} & {3} \\\\".format(filepath, timePerMovement, convergence, int(minmoves))
+
+	
+def loadData(filepath):
+	games = []
+	with open(filepath, 'r') as input:
+		for line in input:
+			games.append(GameData(line.split()))
+			if len(games) >= MAX_GAMES: break
+			
+	return games		
+	
+
+def locate(pattern, root=os.curdir):
+    '''Locate all files matching supplied filename pattern in and below
+    supplied root directory.'''
+    for path, dirs, files in os.walk(os.path.abspath(root)):
+        for filename in fnmatch.filter(files, pattern):
+            yield os.path.join(path, filename)
+			
+def main():
+	for filepath in locate('*-result.out', '../rl.bomber/Corridas'):
+		games = loadData(filepath)
+		file = os.path.basename(filepath)
+		printStats(file, games)
+
+if __name__ == "__main__":
+    main()
